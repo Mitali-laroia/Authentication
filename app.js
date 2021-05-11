0243bf46a96869975a5ddb7fe574adcfe546cff6
Mitali-laroia <04042001mitali@gmail.com>
2021-05-11 11:34:30 +0530

    bug fixing

--- app.js
+++ app.js
@@ -210,7 +210,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "https://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
   },
   function(accessToken, refreshToken, profile, cb) {
 
